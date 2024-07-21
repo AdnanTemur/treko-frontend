5d10b2cac1f4add1eaa57b905895b8bf2e3d0174
noumandatu <noumankhan619.915@gmail.com>
2024-07-21 21:20:07 +0500

    push

--- app/(chat)/employee-chat.tsx
+++ app/(chat)/employee-chat.tsx
@@ -16,7 +16,8 @@ import { useLocalSearchParams } from "expo-router";
 import useAsyncStorage from "@/hooks/useAuth";
 import { Ionicons } from "@expo/vector-icons";
 import { io } from "socket.io-client";
-const socket = io(process.env.EXPO_PUBLIC_SOCKET_URL);
+
+const socket = io(process.env.EXPO_PUBLIC_SOCKET_URL || "");
 
 socket.on("connect", () => {
   console.log("Connected to server");
@@ -27,25 +28,22 @@ socket.on("disconnect", () => {
 });
 
 const EmployeeChat = () => {
-  // hooks
-  const { employeeId } = useLocalSearchParams();
+  const { employeeId }: any = useLocalSearchParams();
   const [accessToken, isLoading]: any = useAsyncStorage("@access_token");
   const [user]: any = useAsyncStorage("@user");
 
-  // state
-  const [messages, setMessages] = useState([]);
+  const [receivedMessages, setReceivedMessages] = useState<any[]>([]);
+  const [sentMessages, setSentMessages] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState<string | null>(null);
   const [newMessage, setNewMessage] = useState("");
   const scrollViewRef = useRef<ScrollView>(null);
 
   useEffect(() => {
     const fetchMessages = async () => {
       if (!employeeId || !accessToken || isLoading) return;
-
       try {
         const jwtToken = accessToken;
-
         const response = await axios.get(
           `${process.env.EXPO_PUBLIC_BACKEND_URL}/coworker-chats/${JSON.parse(
             employeeId
@@ -57,15 +55,27 @@ const EmployeeChat = () => {
             },
           }
         );
-
         const data = response.data;
-        if (data && data.coworkerChats) {
+        if (data && data.coworkerChats && data.coworkerChats.length > 0) {
           const chat = data.coworkerChats[0];
-          setMessages(chat.messageSent);
+          setReceivedMessages(chat.messageReceived || []);
+          setSentMessages(chat.messageSent || []);
+        } else {
+          setReceivedMessages([]);
+          setSentMessages([]);
         }
-      } catch (error) {
+      } catch (error: any) {
+        if (
+          error.response?.status === 404 &&
+          error.response?.data?.message === "No chat found for this user"
+        ) {
+          setError("No chat found for this user.");
+        } else {
+          setError(
+            error.message || "An error occurred while fetching messages."
+          );
+        }
         console.error("Error fetching messages:", error);
-        setError(error.message);
       } finally {
         setLoading(false);
       }
@@ -75,12 +85,19 @@ const EmployeeChat = () => {
   }, [employeeId, accessToken, isLoading]);
 
   useEffect(() => {
-    const handleReceiveMessage = ({ senderId, receiverId, message }) => {
+    const handleReceiveMessage = ({ senderId, receiverId, message }: any) => {
       if (message && message.text) {
-        setMessages((prevMessages) => [
-          ...prevMessages,
-          { senderId, receiverId, ...message },
-        ]);
+        if (senderId === user?._id) {
+          setSentMessages((prevMessages: any) => [
+            ...prevMessages,
+            { senderId, receiverId, ...message },
+          ]);
+        } else {
+          setReceivedMessages((prevMessages: any) => [
+            ...prevMessages,
+            { senderId, receiverId, ...message },
+          ]);
+        }
       }
     };
 
@@ -89,12 +106,11 @@ const EmployeeChat = () => {
     return () => {
       socket.off("receiveMessage", handleReceiveMessage);
     };
-  }, []);
+  }, [user]);
 
   useEffect(() => {
-    // Scroll to bottom on messages update
     scrollViewRef.current?.scrollToEnd({ animated: true });
-  }, [messages]);
+  }, [receivedMessages, sentMessages]);
 
   const sendMessage = () => {
     const senderId = user?._id;
@@ -110,15 +126,15 @@ const EmployeeChat = () => {
 
     socket.emit("sendMessage", {
       senderId,
-      receiverId: JSON.parse(employeeId), // Ensure the employeeId is parsed correctly
+      receiverId: JSON.parse(employeeId),
       messageText: newMessage.trim(),
     });
 
-    setMessages((prevMessages) => [
+    setSentMessages((prevMessages: any) => [
       ...prevMessages,
       {
         senderId,
-        receiverId: JSON.parse(employeeId), // Ensure the employeeId is parsed correctly
+        receiverId: JSON.parse(employeeId),
         text: newMessage.trim(),
         timestamp: new Date(),
       },
@@ -126,6 +142,7 @@ const EmployeeChat = () => {
 
     setNewMessage("");
   };
+
   if (loading || isLoading) {
     return (
       <SafeAreaView style={styles.container}>
@@ -156,22 +173,36 @@ const EmployeeChat = () => {
             scrollViewRef.current?.scrollToEnd({ animated: true })
           }
         >
-          {messages.map((message) => (
-            <View
-              key={message._id || `${message.senderId}-${message.timestamp}`}
-              style={[
-                styles.messageContainer,
-                message.senderId === user?._id
-                  ? styles.sentMessage
-                  : styles.receivedMessage,
-              ]}
-            >
-              <Text>{message.text}</Text>
-              <Text style={styles.timestamp}>
-                {new Date(message.timestamp).toLocaleString()}
-              </Text>
+          {receivedMessages.length === 0 && sentMessages.length === 0 ? (
+            <View style={styles.emptyState}>
+              <Text>No messages to display</Text>
             </View>
-          ))}
+          ) : (
+            <>
+              {receivedMessages.map((message: any, id: any) => (
+                <View
+                  key={id}
+                  style={[styles.messageContainer, styles.receivedMessage]}
+                >
+                  <Text>{message.text}</Text>
+                  <Text style={styles.timestamp}>
+                    {new Date(message.timestamp).toLocaleString()}
+                  </Text>
+                </View>
+              ))}
+              {sentMessages.map((message: any, id: any) => (
+                <View
+                  key={id}
+                  style={[styles.messageContainer, styles.receivedMessage]}
+                >
+                  <Text>{message.text}</Text>
+                  <Text style={styles.timestamp}>
+                    {new Date(message.timestamp).toLocaleString()}
+                  </Text>
+                </View>
+              ))}
+            </>
+          )}
         </ScrollView>
         <View style={styles.inputContainer}>
           <TextInput
@@ -245,146 +276,9 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     padding: 10,
   },
+  emptyState: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
-
-// import React, { useState, useEffect } from "react";
-// import {
-//   View,
-//   Text,
-//   TextInput,
-//   TouchableOpacity,
-//   FlatList,
-//   Alert,
-//   Image,
-// } from "react-native";
-// import { FontAwesome } from "@expo/vector-icons";
-// import useAsyncStorage from "@/hooks/useAuth";
-// import { router, useLocalSearchParams } from "expo-router";
-// import { SafeAreaView } from "react-native-safe-area-context";
-// import AntDesign from "@expo/vector-icons/AntDesign";
-
-// const EmployeeChat = () => {
-
-//   const parsedEmployees = employees ? JSON.parse(employees) : [];
-//   const [message, setMessage] = useState("");
-//   const [messages, setMessages] = useState([]);
-
-//   const employee = parsedEmployees.length > 0 ? parsedEmployees[0] : null;
-
-//   if (!employee) {
-//     return (
-//       <SafeAreaView className="flex-1 justify-center items-center">
-//         <Text>No employee data available.</Text>
-//       </SafeAreaView>
-//     );
-//   }
-
-//   const { _id: employeeId, name: employeeName } = employee;
-
-//   useEffect(() => {
-//     // Fetch messages from the API when the component mounts
-//     const fetchMessages = async () => {
-//       try {
-//         const response = await fetch(
-//           `http://192.168.1.7:8000/api/v1/coworker-chats/${employeeId}/messages`
-//         );
-//         const data = await response.json();
-//         const { coworkerChats } = data;
-//         const messages = [];
-
-//         coworkerChats.forEach((chat) => {
-//           chat.messageSent.forEach((msg) => {
-//             messages.push({
-//               senderId: senderId, // Assuming current user is the sender
-//               receiverId: chat.coworkerId._id,
-//               text: msg.text,
-//               timestamp: msg.timestamp,
-//             });
-//           });
-
-//           chat.messageReceived.forEach((msg) => {
-//             messages.push({
-//               senderId: chat.coworkerId._id,
-//               receiverId: senderId, // Assuming current user is the receiver
-//               text: msg.text,
-//               timestamp: msg.timestamp,
-//             });
-//           });
-//         });
-
-//         setMessages(messages);
-//       } catch (error) {
-//         console.error("Failed to fetch messages:", error);
-//       }
-//     };
-
-//     fetchMessages();
-//   }, [employeeId, senderId]);
-
-//   const renderMessage = ({ item }) => {
-//     const isSender = item.senderId === senderId;
-//     return (
-//       <View
-//         className={`flex-row my-2 ${
-//           isSender ? "justify-start" : "justify-end"
-//         }`}
-//       >
-//         <View
-//           className={`p-3 rounded-lg ${
-//             isSender ? "bg-blue-200" : "bg-green-200"
-//           } max-w-2/3`}
-//         >
-//           <Text>{item.text}</Text>
-//           <Text className="text-xs text-gray-500">
-//             {new Date(item.timestamp).toLocaleTimeString()}
-//           </Text>
-//         </View>
-//       </View>
-//     );
-//   };
-
-//   return (
-//     <SafeAreaView className="h-full bg-white p-4">
-//       <View className="flex-row justify-between items-center mb-10">
-//         <TouchableOpacity onPress={() => router.push("/home")}>
-//           <AntDesign name="arrowleft" size={24} color="black" />
-//         </TouchableOpacity>
-//         <Text className="text-2xl font-bold">Chat with {employeeName}</Text>
-//         <View className="flex items-center">
-//           <Image
-//             source={{
-//               uri:
-//                 user?.avatar ||
-//                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvi7HpQ-_PMSMOFrj1hwjp6LDcI-jm3Ro0Xw&s",
-//             }}
-//             className="w-10 h-10 rounded-full"
-//           />
-//           <Text className="text-[10px]">{user?.name}</Text>
-//         </View>
-//       </View>
-//       <FlatList
-//         data={messages}
-//         keyExtractor={(item, index) => index.toString()}
-//         renderItem={renderMessage}
-//         className="flex-1"
-//         contentContainerStyle={{ paddingBottom: 10, paddingTop: 30 }}
-//       />
-//       <View className="flex-row items-center border-t border-gray-300 p-2">
-//         <TextInput
-//           className="flex-1 border border-gray-300 rounded-l-lg p-3"
-//           placeholder="Type a message"
-//           value={message}
-//           onChangeText={setMessage}
-//         />
-//         <TouchableOpacity
-//           className="bg-blue-500 rounded-r-lg p-3"
-//           onPress={sendMessage}
-//         >
-//           <FontAwesome name="send" size={24} color="white" />
-//         </TouchableOpacity>
-//       </View>
-//     </SafeAreaView>
-//   );
-// };
-
-// export default EmployeeChat;
